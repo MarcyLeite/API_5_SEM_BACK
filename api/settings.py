5d1bfc233b1386f12bc6d4fb4a7424eef672124e
Matheus Marciano <mm.leitea@gmail.com>
2026-03-30 19:23:04 -0300

    feat: feat nova

--- api/settings.py
+++ api/settings.py
@@ -14,6 +14,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 
+print('feat nova')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
